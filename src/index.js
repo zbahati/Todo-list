ffa67b9d6d720ac3614604ffda1f65ffc82441d2
zbahati <bahatizirimwabagabo@gmai.com>
2023-05-11 07:07:46 +0200

    Add interaction checkbox , message when no tasks, Clear completed task functionalities

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import TaskList from './module/TaskList.js';
 const planned = document.querySelector('#tasks_lists');
 const todoInput = document.querySelector('#todo_input');
 const todoBtn = document.querySelector('#todo_btn');
+const clearCompletedBtn = document.querySelector('#clear-completed-btn');
 
-const taskList = new TaskList(planned, todoInput, todoBtn);
+const taskList = new TaskList(planned, todoInput, todoBtn, clearCompletedBtn);
 taskList.init();
