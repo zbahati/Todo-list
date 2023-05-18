91bcdc990fff864ed003904f133003d73579998a
zbahati <bahatizirimwabagabo@gmai.com>
2023-05-18 11:51:36 +0200

    Add mock folder and files

---
+++ src/module/__mocks__/Tasks.test.js
@@ -0,0 +1,23 @@
+
+import taskManager from './localstorage';
+
+describe('Testing ', () => {
+  describe('Add Task Test', () => {
+    test('Add Task', () => {
+      const tasks = taskManager.addNewTask({
+        task: 'Task 1',
+        id: 1,
+        status: false,
+      });
+      expect(tasks).not.toStrictEqual([]);
+    });
+  });
+
+  describe('subtraction function test unit', () => {
+    test('should remove appropriate task upon user actions', () => {
+      expect(
+        taskManager.removeTaskById(1),
+      ).toStrictEqual([]);
+    });
+  });
+});
