4a18be1fd4253bdfa1fec198719eba4feba828d1
zbahati <bahatizirimwabagabo@gmai.com>
2023-05-18 16:57:43 +0200

    Correct linters error

--- src/module/__mocks__/Tasks.test.js
+++ src/module/__mocks__/Tasks.test.js
@@ -9,23 +9,22 @@ describe('Testing ', () => {
         status: false,
       });
 
-      document.body.innerHTML =
-      '<div>' +
-      '  <ul id="list"></ul>' +
-      '</div>';
+      document.body.innerHTML = '<div>'
+      + '  <ul id="list"></ul>'
+      + '</div>';
 
-      const ullem  = document.getElementById('list');
-      let lielem  = document.createElement('li');
-      lielem.innerHTML=tasks.task
+      const ullem = document.getElementById('list');
+      const lielem = document.createElement('li');
+      lielem.innerHTML = tasks.task;
       ullem.appendChild(lielem);
 
       expect(tasks).not.toStrictEqual([]);
     });
   });
 
-  test('Add one new item to the list', () => {    
-      const list = document.querySelectorAll('#list li');
-      expect(list).toHaveLength(1);
+  test('Add one new item to the list', () => {
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
   });
 
   describe('subtraction function test unit', () => {
